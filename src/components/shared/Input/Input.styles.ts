5521391aa7e06d4597057ecc0dba3dd4b57aedc5
Felipe Henrique <felipehfsouza@gmail.com>
2021-06-16 21:38:07 -0300

    CustomInput added

---
+++ src/components/shared/Input/Input.styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div``;
+export const Label = styled.label`
+    font-size: 16px;
+`;
+export const FormControl = styled.input`
+    margin-top: 8px;
+    display: block;
+    border: none;
+    width: 100%;
+    padding: 14px;
+    background-color: #e6e6e6;
+`;
+
+export const Warning = styled.small`
+    color: red;
+`
